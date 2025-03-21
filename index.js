d810597a769d97480b02ff34521c4bfaeaacf4c9
prasanna97949 <balaprasanna621@gmail.com>
2025-03-21 14:03:32 +0530

    index updated

--- index.js
+++ index.js
@@ -38,3 +38,15 @@ setInterval(() => {
   moveSlide();
 }, 3000);
 
+// faq
+const faqButtons = document.querySelectorAll('.faq-btn');
+
+faqButtons.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    const content = btn.nextElementSibling;
+    const icon = btn.querySelector('.faq-icon');
+
+    content.classList.toggle('hidden');
+    icon.textContent = content.classList.contains('hidden') ? '+' : '-';
+  });
+});
